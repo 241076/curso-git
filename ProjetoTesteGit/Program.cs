14b931215fda005a0f5d21d6d93f90db8e2833af
Mauricio Gomes <gomes.mauricio@gmail.com>
2021-03-11 23:24:38 -0300

    acresentei msg boa tarde

--- ProjetoTesteGit/Program.cs
+++ ProjetoTesteGit/Program.cs
@@ -7,6 +7,7 @@ namespace ProjetoTesteGit
         static void Main(string[] args)
         {
             Console.WriteLine("Ola Mundo!");
+            Console.WriteLine("boa tarde");
 
             Console.ReadLine();
         }
